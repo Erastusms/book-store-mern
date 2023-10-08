9b7c429e9330d834c4f441ec75a07fae7d6c9219
Erastus Mauliate <emauliate17@gmail.com>
2023-10-08 15:43:39 +0700

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb://admin:admin@ac-2jcfvlj-shard-00-00.nemoveh.mongodb.net:27017,ac-2jcfvlj-shard-00-01.nemoveh.mongodb.net:27017,ac-2jcfvlj-shard-00-02.nemoveh.mongodb.net:27017/books-collection?ssl=true&replicaSet=atlas-2bzsn4-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
